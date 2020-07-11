5a7fb72009bb24d900a973ddacace2ab26f5175e
nategoethel <44580393+nategoethel@users.noreply.github.com>
2020-07-11 12:12:19 -0500

    Implemented first draft of search() function in BTree.java

--- skeleton-code/BTree.java
+++ skeleton-code/BTree.java
@@ -1,71 +1,152 @@
 /**
- * Do NOT modify.
- * This is the class with the main function
+ * Do NOT modify. This is the class with the main function
  */
 
 import java.util.ArrayList;
 import java.util.List;
+import application.BTree.BTreeNode;
 
 /**
- * B+Tree Structure
- * Key - StudentId
- * Leaf Node should contain [ key,recordId ]
+ * B+Tree Structure Key - StudentId Leaf Node should contain [ key,recordId ]
  */
 class BTree {
 
+  /**
+   * Pointer to the root node.
+   */
+  private BTreeNode root;
+  /**
+   * Number of key-value pairs allowed in the tree/the minimum degree of B+Tree
+   **/
+  private int t;
+
+  BTree(int t) {
+    this.root = null;
+    this.t = t;
+  }
+
+  long search(long studentId) {
     /**
-     * Pointer to the root node.
+     * TODO: Implement this function to search in the B+Tree. Return recordID for the given
+     * StudentID. Otherwise, print out a message that the given studentId has not been found in the
+     * table and return -1.
      */
-    private BTreeNode root;
+
+    BTreeNode foundNode = search(this.root, studentId);
+    
+    if (foundNode == null) {
+      System.out.println("The provided ID has not been found in the table.");
+      return -1;
+    } else {
+      int index = foundNode.keys.indexOf(studentId);
+      return foundNode.values.get(index);
+    }
+
+
+    //return -1;
+  }
+
+  private BTreeNode search(BTreeNode current, long studentId) {
+
+    // check whether the current node has the key
+    for (int i = 0; i < current.keys.size(); i++) {
+      if (current.keys.get(i) == studentId) {
+        return current;
+      } 
+    }
+    
+    // if the current node isn't a leaf node, find the child node to move to.
+    if (!current.leaf) {
+      int i = 1;
+      while (i <= current.getNumKeys() && studentId > current.keys.get(i - 1)) {
+        i++;
+      }
+      return search(current.children.get(i - 1), studentId);
+    } else { // if it is a leaf, the id doesn't exist
+      return null;
+    }
+
+
+
+    //return null;
+  }
+
+  BTree insert(Student student) {
     /**
-     * Number of key-value pairs allowed in the tree/the minimum degree of B+Tree
-     **/
-    private int t;
+     * TODO: Implement this function to insert in the B+Tree. Also, insert in student.csv after
+     * inserting in B+Tree.
+     */
 
-    BTree(int t) {
-        this.root = null;
-        this.t = t;
+    BTreeNode current = this.root;
+
+    if (current == null) {
+      current = new BTreeNode(t, true);
+      // TODO write to CSV
+      return this;
     }
 
-    long search(long studentId) {
-        /**
-         * TODO:
-         * Implement this function to search in the B+Tree.
-         * Return recordID for the given StudentID.
-         * Otherwise, print out a message that the given studentId has not been found in the table and return -1.
-         */
-        return -1;
+    // check whether the Student exists in the tree
+    return this;
+  }
+
+  boolean delete(long studentId) {
+    /**
+     * TODO: Implement this function to delete in the B+Tree. Also, delete in student.csv after
+     * deleting in B+Tree, if it exists. Return true if the student is deleted successfully
+     * otherwise, return false.
+     */
+    return true;
+  }
+
+  List<Long> print() {
+
+    List<Long> listOfRecordID = new ArrayList<>();
+
+    /**
+     * TODO: Implement this function to print the B+Tree. Return a list of recordIDs from left to
+     * right of leaf nodes.
+     *
+     */
+
+    // check whether the root is null
+    if (this.root == null) {
+      System.out.println("The B+ tree is empty!");
     }
 
-    BTree insert(Student student) {
-        /**
-         * TODO:
-         * Implement this function to insert in the B+Tree.
-         * Also, insert in student.csv after inserting in B+Tree.
-         */
-        return this;
+    System.out.println("------------------------------------------");
+    print(this.root, "");
+    System.out.println("------------------------------------------");
+
+
+    return listOfRecordID;
+  }
+
+  /**
+   * private recursive helper method for printSideways() above.
+   * 
+   * @param current the node to print from
+   * @param indent
+   */
+  private void print(BTreeNode current, String indent) {
+    if (current != null) {
+      if (current.getNumChildren() == 1) {
+        print(current.children.get(0), indent + "    ");
+      } else if (current.getNumChildren() == 2) {
+        print(current.children.get(1), indent + "    ");
+        System.out.println(indent + current.getKeys());
+        print(current.children.get(0), indent + "    ");
+      } else if (current.getNumChildren() == 3) {
+        print(current.children.get(2), indent + "    ");
+        System.out.println(indent + current.getKeys());
+        print(current.children.get(1), indent + "    ");
+        print(current.children.get(0), indent + "    ");
+      } else {
+        print(current.children.get(3), indent + "    ");
+        print(current.children.get(2), indent + "    ");
+        System.out.println(indent + current.getKeys());
+        print(current.children.get(1), indent + "    ");
+        print(current.children.get(0), indent + "    ");
+      }
     }
-
-    boolean delete(long studentId) {
-        /**
-         * TODO:
-         * Implement this function to delete in the B+Tree.
-         * Also, delete in student.csv after deleting in B+Tree, if it exists.
-         * Return true if the student is deleted successfully otherwise, return false.
-         */
-        return true;
-    }
-
-    List<Long> print() {
-
-        List<Long> listOfRecordID = new ArrayList<>();
-
-        /**
-         * TODO:
-         * Implement this function to print the B+Tree.
-         * Return a list of recordIDs from left to right of leaf nodes.
-         *
-         */
-        return listOfRecordID;
-    }
+  }
 }
--- skeleton-code/BTreeNode.java
+++ skeleton-code/BTreeNode.java
@@ -6,12 +6,12 @@ class BTreeNode {
      * Array of the keys stored in the node.
      */
     //long[] keys;
-    ArrayList<Long> keys = new ArrayList<Long>();
+    ArrayList<Long> keys;
     /**
      * Array of the values[recordID] stored in the node. This will only be filled when the node is a leaf node.
      */
     //long[] values;
-    ArrayList<Long> values = new ArrayList<Long>();
+    ArrayList<Long> values;
     /**
      * Minimum degree (defines the range for number of keys)
      **/
@@ -21,7 +21,7 @@ class BTreeNode {
      * this node is a leaf, then null.
      */
     //BTreeNode[] children;
-    ArrayList<BTreeNode> children = new ArrayList<>();
+    ArrayList<BTreeNode> children;
     /**
      * number of key-value pairs in the B-tree
      */
@@ -40,10 +40,35 @@ class BTreeNode {
     BTreeNode(int t, boolean leaf) {
         this.t = t;
         this.leaf = leaf;
-        this.keys = new long[2 * t - 1];
-        this.children = new BTreeNode[2 * t];
+        this.keys = new ArrayList<>();
+        //this.keys = new long[2 * t - 1];
+        //this.children = new BTreeNode[2 * t];
+        this.children = new ArrayList<>();
         this.n = 0;
         this.next = null;
-        this.values = new long[2 * t - 1];
+        //this.values = new long[2 * t - 1];
+        this.values = new ArrayList<>();
+    }
+    
+    /**
+     * 
+     * @return The number of child nodes this node has
+     */
+    public int getNumChildren() {
+      
+      return this.children.size();
+    }
+    
+    /**
+     * 
+     * @return The number of keys this node has
+     */
+    public int getNumKeys() {
+      
+      return this.keys.size();
+    }
+    
+    public ArrayList<Long> getKeys(){
+      return this.keys;
     }
 }
